c5215e3cdb5ce93565ea5eab78821015c68113d6
Ginika Elizabeth Nna <elizabethginika@gmail.com>
2024-06-21 17:42:03 -0700

    created Amenity class that inherited from BaseModel, initialized and returned in string format

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from .base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Amenity inherits from BaseModel """
+    def __init__(self, *args, **kwargs):
+        """ Initializes amenity instance """
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
+
+    def __str__(self):
+        """ Retrns String representation of Amenity Instance """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
