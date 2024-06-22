9ad1e544b44848a06e8c080cf94b6bcff78d7f4e
Ginika Elizabeth Nna <elizabethginika@gmail.com>
2024-06-21 17:31:46 -0700

    created City class that inherited from BaseModel, initialized and returned in string format

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from .base_model import BaseModel
+
+class City(BaseModel):
+    """ City inherits from BaseModel """
+    def __init__(self, *args, **kwargs):
+        """ Initializes City instance """
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
+
+    def __str__(self):
+        """ Returns the string representation of the City Instance """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
