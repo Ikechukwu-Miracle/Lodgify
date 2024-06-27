5315125583893f1ec3962c748e0100f943745946
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-06-27 23:32:59 +0100

    fix: handle keyboard interrupt error when '^ + C' is typed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """FileStorage class"""
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+
 
 
 class FileStorage:
