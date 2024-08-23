aab6ff630745f3ed9f776ff4aba44f4d00ff08f0
Ginika Elizabeth Nna <elizabethginika@gmail.com>
2024-08-23 15:44:49 -0700

    imported the header path to the app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+	<Header />
         <Footer />
       </BrowserRouter>
     </div>
