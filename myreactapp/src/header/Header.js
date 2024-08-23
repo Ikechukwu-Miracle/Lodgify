e20d62eadee32c5ca5168f13d785afbe10cbfce7
Ginika Elizabeth Nna <elizabethginika@gmail.com>
2024-08-22 22:00:39 -0700

    header icons

---
+++ myreactapp/src/header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Header.css';
+import { FaSearch, FaGlobe, FaBars, FaUserCircle } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img 
+          src="https://res.cloudinary.com/dy2aom8f0/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1723304345/Pink_and_Black_Modern_Initials_Logo_Design__3_-removebg-preview_d7cs7a.png" 
+          alt="Logo" 
+        />
+      </div>
+      
+      <div className="header__search">
+        <div className="search__categories">
+          <button className="category__button">Available</button>
+          <button className="category__button">Experience</button>
+        </div>
+        <div className="search__inputs">
+          <input type="text" placeholder="Location" className="search__input" />
+          <input type="date" placeholder="Arrival" className="search__input" />
+          <input type="date" placeholder="Departure" className="search__input" />
+          <input type="number" placeholder="Guest Members" className="search__input" min="1" />
+        </div>
+        <button className="search__button">
+          <FaSearch />
+        </button>
+      </div>
+      
+      <div className="header__right">
+        <a href="#" className="header__link">Lodgify, Home Anywhere</a>
+        <FaGlobe className="header__icon" />
+        <div className="header__profile">
+          <FaBars className="header__icon" />
+          <FaUserCircle className="header__icon" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
